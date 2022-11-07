4f46a96175069dbdfb1dcf42a10216901a7b9948
jeffsbarbosa <jbviolao@gmail.com>
2022-11-07 20:56:56 -0300

    Testes em Cypress

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,49 @@
+import SignupPage from '../PAGES/SignupPage'
+import signup from '../PAGES/SignupPage'
+
+describe('Cadastro', () => {
+
+    beforeEach(function () {
+        cy.fixture('deliver.json').then((d) => {
+            this.deliver = d
+        })
+    })
+
+    it('Usuário deve se tornar um entregador', function () {
+        signup.go()
+        signup.fillForm(this.deliver.signup)
+        signup.submit()
+
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        signup.modalContentShouldBe(expectedMessage)
+    })
+
+    it('CPF incorreto', function () {
+        signup.go()
+        signup.fillForm(this.deliver.cpf_inv)
+        signup.submit()
+        signup.alertMessageShouldBe('Oops! CPF inválido')
+
+    })
+
+    it('Email inválido', function () {
+        signup.go()
+        signup.fillForm(this.deliver.email_inv)
+        signup.submit()
+        signup.alertMessageShouldBe('Oops! Email com formato inválido.')
+    })
+
+    it('Campos Obrigatórios', function () {
+        signup.go()
+        signup.submit()
+        signup.alertMessageShouldBe('É necessário informar o nome')
+        signup.alertMessageShouldBe('É necessário informar o CPF')
+        signup.alertMessageShouldBe('É necessário informar o email')
+        signup.alertMessageShouldBe('É necessário informar o CEP')
+        signup.alertMessageShouldBe('É necessário informar o número do endereço')
+        signup.alertMessageShouldBe('Selecione o método de entrega')
+        signup.alertMessageShouldBe('Adicione uma foto da sua CNH')
+    })
+
+})
+//teste
